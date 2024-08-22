6c188e16496c9825ce83ccfe6e6cad2975ec2ac9
Sergii Stoiachko <119351759+SergiiSt@users.noreply.github.com>
2024-08-22 16:04:01 +0200

    start

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,9 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  const contacts = Array(number).fill(0).map(createFakeContact);
+  console.log(contacts);
+};
 
 generateContacts(5);
